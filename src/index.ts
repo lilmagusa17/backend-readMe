9862c9c382a33610ec3340ab71f0d1507a68e277
Vanessa Sanchez Morales <vasamo345@gmail.com>
2025-09-09 15:04:06 -0500

    feat: added express, modified index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Holiiiii");
+import express, {Express, Request, Response} from 'express';
+import {db} from './config/connectionDB'
+import { bookRouter } from './routes';
+
+const app: Express = express();
+
+process.loadEnvFile();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); //Content-type: json
+app.use(express.urlencoded({extended: true})); //para cuando mando archivos en el host, para que pueda recibirlos
+
+app.use("/api/books", bookRouter.router);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Holiiii');
+});
+
+db.then( () => 
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+}) )
+
