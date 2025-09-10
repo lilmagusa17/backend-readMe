a03ec99a9a4a2c524705b121b7a6711c0c8bf757
Vanessa Sanchez Morales <vasamo345@gmail.com>
2025-09-10 13:17:56 -0500

    feat: update method for books created

--- src/index.ts
+++ src/index.ts
@@ -21,4 +21,3 @@ db.then( () =>
     app.listen(PORT, ()=>{
         console.log(`Server is running on port ${PORT}`)
 }) )
-
--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,4 +1,4 @@
-import { BookInput } from "../interfaces";
+import { BookInput, BookInputUpdate } from "../interfaces";
 import { BookDocument, BookModel } from "../models";
 
 class BookService{
@@ -14,6 +14,44 @@ class BookService{
 
     public findByTitle(title: string): Promise<BookDocument | null>{
         return BookModel.findOne({ title });
+    }
+
+    public async findAll(): Promise<BookDocument[]> {
+        
+        try{
+            return BookModel.find({});
+        }catch(error){
+            throw error;
+        }
+    }
+
+    public async delete(title: string){
+        try {
+            const bookExists: BookDocument | null = await this.findByTitle(title);
+            if (bookExists === (null)){
+                throw new ReferenceError("Book doesn't exists");
+            }
+            return BookModel.deleteOne({title: title});
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    public async update(title: string, bookInput: BookInputUpdate){
+        try{
+            const bookExists: BookDocument | null = await this.findByTitle(title);
+            if (bookExists === (null)){
+                console.log("noooo");
+                throw new ReferenceError("Book doesn't exists");
+            }
+            const book: BookDocument | null = await BookModel.findOneAndUpdate({title: title}, bookInput, {returnOriginal: false});
+            
+            //console.log(book);
+            return book;
+        }catch(error){
+            throw error;
+        }
     }
 
 }
